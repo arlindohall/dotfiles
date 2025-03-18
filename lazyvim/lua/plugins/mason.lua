8a264b99c47731d69d88397a60fec3268d4a927d
Miller Arlindo Hall <hall@hey.com>
2025-03-17 22:00:59 -0400

    feat: Add prettier formatting back into Conform

--- lazyvim/lua/plugins/mason.lua
+++ lazyvim/lua/plugins/mason.lua
@@ -11,6 +11,24 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
