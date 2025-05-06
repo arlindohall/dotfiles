b27b98a7fb81117e1db37aa78c32ed7e923cde19
Miller Arlindo Hall <miller.hall@shopify.com>
2025-05-06 11:44:04 -0400

    feat: Use raw lsp commands and fix environments instead

--- lazyvim/lua/plugins/mason.lua
+++ lazyvim/lua/plugins/mason.lua
@@ -1,23 +1,4 @@
 -- Function to check if 'bundle exec' works in the current working directory
-local function is_bundle_exec_available(cwd)
-  local result = vim.fn.trim(vim.fn.system("cd " .. cwd .. " && bundle exec ruby -v"))
-  return vim.v.shell_error == 0 and result ~= ""
-end
-
-local function get_ruby_cmd(cmd)
-  local cwd = vim.fn.getcwd()
-  local result = {}
-  if is_bundle_exec_available(cwd) then
-    result = { "bundle", "exec" }
-  end
-
-  for _, v in ipairs(cmd) do
-    table.insert(result, v)
-  end
-
-  return result
-end
-
 return {
   {
     "williamboman/mason.nvim",
@@ -54,12 +35,12 @@ return {
       servers = {
         ruby_lsp = {
           mason = false,
-          cmd = get_ruby_cmd({ "ruby-lsp" }),
+          cmd = { "ruby-lsp" },
           filetypes = { "ruby" },
         },
         sorbet = {
           mason = false,
-          cmd = get_ruby_cmd({ "srb", "tc", "--lsp" }),
+          cmd = { "srb", "tc", "--lsp" },
           init_options = {
             enableTypedFalseCompletionNudges = true,
           },
@@ -67,7 +48,7 @@ return {
         },
         rubocop = {
           mason = false,
-          cmd = get_ruby_cmd({ "rubocop", "--lsp" }),
+          cmd = { "rubocop", "--lsp" },
           filetypes = { "ruby" },
         },
       },
