3598fe6f7d4085b8b6081b67a58803ab347a108a
Miller Arlindo Hall <miller.hall@shopify.com>
2025-06-06 22:50:12 -0400

    fix(git): JJ style commands aware if HEAD is a jj style commit

--- bin/lib/git_jj.rb
+++ bin/lib/git_jj.rb
@@ -21,19 +21,22 @@ def setup # rubocop:disable Metrics/PerceivedComplexity
   # date:   fri jun 6 18:46:45 2025 -0400
   show = `git show --stat`
   author = /Author: ([\w\s]+)<(.*?)>/.match(show)&.captures&.last&.to_s
-  head_message = /    \w+/.match(show)&.to_s
+  head_message = /^    .+/.match(show)&.to_s&.strip
   has_no_diff = `git diff HEAD`.empty?
 
   is_wip = head_message&.start_with?('wip:') == true
   is_me = author == 'miller.hall@shopify.com'
 
-  system 'git add .'
-  if is_wip && is_me
-    system 'git commit --amend --no-edit'
-  elsif !has_no_diff
-    system %(git commit --allow-empty -m 'wip: Committing working tree')
-  end
-
   @message = 'Committing working tree' if @message.empty?
   @message = "wip: #{@message}"
+  @head_not_on_jj_style_commit = false
+
+  system 'git add .'
+  if is_wip && is_me
+    system 'git commit --allow-empty --amend --no-edit'
+  elsif !has_no_diff
+    system %(git commit --allow-empty -m '#{@message}')
+  else
+    @head_not_on_jj_style_commit = true
+  end
 end
