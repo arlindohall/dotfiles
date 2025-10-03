0c3063711863256ee539e0e605557bca5b673df2
Miller Arlindo Hall <miller.hall@shopify.com>
2025-10-03 12:51:40 -0400

    fix: move keymaps into a plugin so they load later in the cycle

---
+++ lazyvim/lua/plugins/fzf.lua
@@ -0,0 +1,7 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    { "<leader><space>", "<cmd>FzfLua files cwd<CR>", desc = "Find Files (cwd) - my remap for relative files" },
+    { "<leader>/", "<cmd>FzfLua grep cwd<CR>", desc = "Grep (cwd) - my remap for relative files" },
+  },
+}
