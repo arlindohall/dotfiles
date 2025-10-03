89e5cb1738ade2b96d33a9714f58f7d93db33205
Miller Arlindo Hall <miller.hall@shopify.com>
2025-10-03 12:55:21 -0400

    fix: have quick search comand double-enter to open window

--- lazyvim/lua/plugins/fzf.lua
+++ lazyvim/lua/plugins/fzf.lua
@@ -2,6 +2,6 @@ return {
   "ibhagwan/fzf-lua",
   keys = {
     { "<leader><space>", "<cmd>FzfLua files cwd<CR>", desc = "Find Files (cwd) - my remap for relative files" },
-    { "<leader>/", "<cmd>FzfLua grep cwd<CR>", desc = "Grep (cwd) - my remap for relative files" },
+    { "<leader>/", "<cmd>FzfLua grep cwd<CR><CR>", desc = "Grep (cwd) - my remap for relative files" },
   },
 }
