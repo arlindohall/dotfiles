4823e1398fd8a48f0dcd5b2a9cb512e2fef5f939
Miller Arlindo Hall <miller.hall@shopify.com>
2026-02-09 10:51:58 -0500

    feat: use claude to write a wrapper for lsp commands for mason

--- lazyvim/lua/plugins/mason.lua
+++ lazyvim/lua/plugins/mason.lua
@@ -1,4 +1,24 @@
--- Function to check if 'bundle exec' works in the current working directory
+local function debug_print(_msg)
+  -- print(_msg)
+end
+
+local function in_world_monorepo(path)
+  return path and string.find(path, "world/trees") ~= nil
+end
+
+local function wrap_cmd_bundle_exec(cmd, root_dir)
+  if in_world_monorepo(root_dir) then
+    local wrapped = { "bundle", "exec" }
+    for _, arg in ipairs(cmd) do
+      table.insert(wrapped, arg)
+    end
+    debug_print("[LSP DEBUG] World detected! Wrapping with bundle exec: " .. vim.inspect(wrapped))
+    return wrapped
+  end
+  debug_print("[LSP DEBUG] Not in World, using plain command: " .. vim.inspect(cmd))
+  return cmd
+end
+
 return {
   {
     "mason-org/mason.nvim",
@@ -34,6 +54,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        -- Ruby-lsp doesn't have a setup callback because it doesn't work (not sure why),
+        -- but it *DOES* need to be installed with `gem install ruby_lsp` in Core to work,
+        -- again, not really sure why but it works.
         ruby_lsp = {
           mason = false,
           cmd = { "ruby-lsp" },
@@ -52,6 +75,34 @@ return {
           cmd = { "rubocop", "--lsp" },
           filetypes = { "ruby" },
         },
+      },
+      setup = {
+        sorbet = function(_, opts)
+          debug_print("[LSP DEBUG] Setting up sorbet")
+          local lspconfig = require("lspconfig")
+
+          opts.on_new_config = function(config, root_dir)
+            debug_print("[LSP DEBUG] sorbet on_new_config called with root_dir: " .. (root_dir or "nil"))
+            config.cmd_cwd = root_dir
+            config.cmd = wrap_cmd_bundle_exec({ "srb", "tc", "--lsp" }, root_dir)
+          end
+
+          lspconfig.sorbet.setup(opts)
+          return true -- Prevent default setup
+        end,
+        rubocop = function(_, opts)
+          debug_print("[LSP DEBUG] Setting up rubocop")
+          local lspconfig = require("lspconfig")
+
+          opts.on_new_config = function(config, root_dir)
+            debug_print("[LSP DEBUG] rubocop on_new_config called with root_dir: " .. (root_dir or "nil"))
+            config.cmd_cwd = root_dir
+            config.cmd = wrap_cmd_bundle_exec({ "rubocop", "--lsp" }, root_dir)
+          end
+
+          lspconfig.rubocop.setup(opts)
+          return true -- Prevent default setup
+        end,
       },
     },
   },
