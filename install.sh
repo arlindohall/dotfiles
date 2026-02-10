f1d0a95ebf152a35c0ed319d34d780a209a43c82
Miller Arlindo Hall <miller.hall@shopify.com>
2026-02-10 14:45:11 -0500

    fix: additional zsh changes to get the kubectl defaults working

--- install.sh
+++ install.sh
@@ -88,8 +88,7 @@ function install_work_mac {
   rc_install tmux/conf .tmux.conf
   rc_install tmux/conf_local .tmux.conf.local
   rc_install vim/vimrc .vimrc
-  rc_install zsh/work_zshrc .config/.work_zshrc
-  rc_install zsh/mac_zshrc .zshrc
+  rc_install zsh/work_zshrc .zshrc
 
   install_lazyvim_config
 }
